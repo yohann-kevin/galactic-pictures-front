234dfd3474f331bbac5bb8c9bc5d78fba4dad61e
Kirua <yo44prg@icloud.com>
2021-09-13 09:53:02 +0200

    feat: change celle by image

--- src/components/Galery.vue
+++ src/components/Galery.vue
@@ -1,47 +1,36 @@
 <template>
   <div class="scene">
-  <div class="carousel">
-    <div class="carousel__cell">1</div>
-    <div class="carousel__cell">2</div>
-    <div class="carousel__cell">3</div>
-    <div class="carousel__cell">4</div>
-    <div class="carousel__cell">5</div>
-    <div class="carousel__cell">6</div>
-    <div class="carousel__cell">7</div>
-    <div class="carousel__cell">8</div>
-    <div class="carousel__cell">9</div>
-    <div class="carousel__cell">10</div>
-    <div class="carousel__cell">11</div>
-    <div class="carousel__cell">12</div>
-    <div class="carousel__cell">13</div>
-    <div class="carousel__cell">14</div>
-    <div class="carousel__cell">15</div>
+    <div class="carousel">
+      <div class="carousel__cell"><img class="cell-image" src="../assets/images/LastRingPortrait_Cassini_1080.jpg" /></div>
+      <div class="carousel__cell"><img class="cell-image" src="../assets/images/LastRingPortrait_Cassini_1080.jpg" /></div>
+      <div class="carousel__cell"><img class="cell-image" src="../assets/images/LastRingPortrait_Cassini_1080.jpg" /></div>
+      <div class="carousel__cell"><img class="cell-image" src="../assets/images/LastRingPortrait_Cassini_1080.jpg" /></div>
+      <div class="carousel__cell"><img class="cell-image" src="../assets/images/LastRingPortrait_Cassini_1080.jpg" /></div>
+      <div class="carousel__cell"><img class="cell-image" src="../assets/images/LastRingPortrait_Cassini_1080.jpg" /></div>
+      <div class="carousel__cell"><img class="cell-image" src="../assets/images/LastRingPortrait_Cassini_1080.jpg" /></div>
+      <div class="carousel__cell"><img class="cell-image" src="../assets/images/LastRingPortrait_Cassini_1080.jpg" /></div>
+      <div class="carousel__cell"><img class="cell-image" src="../assets/images/LastRingPortrait_Cassini_1080.jpg" /></div>
+      <div class="carousel__cell"><img class="cell-image" src="../assets/images/LastRingPortrait_Cassini_1080.jpg" /></div>
+    </div>
   </div>
-</div>
 
-<div class="carousel-options">
-  <p>
-    <label>
-      Cells
-      <input class="cells-range" type="range" min="3" max="15" value="9" />
-    </label>
-  </p>
-  <p>
-    <button class="previous-button">Previous</button>
-    <button class="next-button">Next</button>
-  </p>
-  <p>
-    Orientation:
-    <label>
-      <input type="radio" name="orientation" value="horizontal" checked />
-      horizontal
-    </label>
-    <label>
-      <input type="radio" name="orientation" value="vertical" />
-      vertical
-    </label>
-  </p>
-</div>
+  <div class="carousel-options">
+    <p>
+      <button class="previous-button">Previous</button>
+      <button class="next-button">Next</button>
+    </p>
+    <p>
+      Orientation:
+      <label>
+        <input type="radio" name="orientation" value="horizontal" checked />
+        horizontal
+      </label>
+      <label>
+        <input type="radio" name="orientation" value="vertical" />
+        vertical
+      </label>
+    </p>
+  </div>
 </template>
 
 <script>
@@ -63,8 +52,7 @@ export default {
 
       function rotateCarousel() {
         var angle = theta * selectedIndex * -1;
-        carousel.style.transform = 'translateZ(' + -radius + 'px) ' + 
-          rotateFn + '(' + angle + 'deg)';
+        carousel.style.transform = 'translateZ(' + -radius + 'px) ' + rotateFn + '(' + angle + 'deg)';
       }
 
       var prevButton = document.querySelector('.previous-button');
@@ -79,50 +67,43 @@ export default {
         rotateCarousel();
       });
 
-    var cellsRange = document.querySelector('.cells-range');
-    cellsRange.addEventListener( 'change', changeCarousel );
-    cellsRange.addEventListener( 'input', changeCarousel );
-
-    function changeCarousel() {
-      cellCount = cellsRange.value;
-      theta = 360 / cellCount;
-      var cellSize = isHorizontal ? cellWidth : cellHeight;
-      radius = Math.round( ( cellSize / 2) / Math.tan( Math.PI / cellCount ) );
-      for ( var i=0; i < cells.length; i++ ) {
-        var cell = cells[i];
-        if ( i < cellCount ) {
-          // visible cell
-          cell.style.opacity = 1;
-          var cellAngle = theta * i;
-          cell.style.transform = rotateFn + '(' + cellAngle + 'deg) translateZ(' + radius + 'px)';
-        } else {
-          // hidden cell
-          cell.style.opacity = 0;
-          cell.style.transform = 'none';
+      function changeCarousel() {
+        cellCount = 10;
+        theta = 360 / cellCount;
+        var cellSize = isHorizontal ? cellWidth : cellHeight;
+        radius = Math.round( ( cellSize / 2) / Math.tan( Math.PI / cellCount ) );
+        for ( var i=0; i < cells.length; i++ ) {
+          var cell = cells[i];
+          if ( i < cellCount ) {
+            cell.style.opacity = 1;
+            var cellAngle = theta * i;
+            cell.style.transform = rotateFn + '(' + cellAngle + 'deg) translateZ(' + radius + 'px)';
+          } else {
+            cell.style.opacity = 0;
+            cell.style.transform = 'none';
+          }
         }
+
+        rotateCarousel();
       }
 
-      rotateCarousel();
-    }
+      var orientationRadios = document.querySelectorAll('input[name="orientation"]');
+      ( function() {
+        for ( var i=0; i < orientationRadios.length; i++ ) {
+          var radio = orientationRadios[i];
+          radio.addEventListener( 'change', onOrientationChange );
+        }
+      })();
 
-    var orientationRadios = document.querySelectorAll('input[name="orientation"]');
-    ( function() {
-      for ( var i=0; i < orientationRadios.length; i++ ) {
-        var radio = orientationRadios[i];
-        radio.addEventListener( 'change', onOrientationChange );
+      function onOrientationChange() {
+        var checkedRadio = document.querySelector('input[name="orientation"]:checked');
+        isHorizontal = checkedRadio.value == 'horizontal';
+        rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
+        document.getElementsByClassName("scene")[0].style.marginTop = isHorizontal ? '70px' : '300px' ;
+        changeCarousel();
       }
-    })();
 
-    function onOrientationChange() {
-      var checkedRadio = document.querySelector('input[name="orientation"]:checked');
-      isHorizontal = checkedRadio.value == 'horizontal';
-      rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
-      changeCarousel();
-    }
-
-    // set initials
-    onOrientationChange();
-
+      onOrientationChange();
     }
   },
 }
@@ -131,11 +112,10 @@ export default {
 <style>
 .scene {
   border: 1px solid #CCC;
-  margin: 40px 0;
   position: relative;
-  width: 210px;
-  height: 140px;
-  margin: 80px auto;
+  width: 840px;
+  height: 560px;
+  margin-top: 70px;
   perspective: 1000px;
 }
 
@@ -143,47 +123,23 @@ export default {
   width: 100%;
   height: 100%;
   position: absolute;
-  transform: translateZ(-288px);
   transform-style: preserve-3d;
   transition: transform 1s;
 }
 
 .carousel__cell {
   position: absolute;
-  width: 190px;
-  height: 120px;
-  left: 10px;
-  top: 10px;
+  width: 760px;
+  height: 480px;
+  left: 20px;
+  top: 20px;
   border: 2px solid black;
-  line-height: 116px;
-  font-size: 80px;
+  line-height: 232px;
+  font-size: 160px;
   font-weight: bold;
-  color: white;
   text-align: center;
   transition: transform 1s, opacity 1s;
 }
-
-.carousel__cell:nth-child(9n+1) { background: hsla(  0, 100%, 50%, 0.8); }
-.carousel__cell:nth-child(9n+2) { background: hsla( 40, 100%, 50%, 0.8); }
-.carousel__cell:nth-child(9n+3) { background: hsla( 80, 100%, 50%, 0.8); }
-.carousel__cell:nth-child(9n+4) { background: hsla(120, 100%, 50%, 0.8); }
-.carousel__cell:nth-child(9n+5) { background: hsla(160, 100%, 50%, 0.8); }
-.carousel__cell:nth-child(9n+6) { background: hsla(200, 100%, 50%, 0.8); }
-.carousel__cell:nth-child(9n+7) { background: hsla(240, 100%, 50%, 0.8); }
-.carousel__cell:nth-child(9n+8) { background: hsla(280, 100%, 50%, 0.8); }
-.carousel__cell:nth-child(9n+0) { background: hsla(320, 100%, 50%, 0.8); }
-
-.carousel__cell:nth-child(1) { transform: rotateY(  0deg) translateZ(288px); }
-.carousel__cell:nth-child(2) { transform: rotateY( 40deg) translateZ(288px); }
-.carousel__cell:nth-child(3) { transform: rotateY( 80deg) translateZ(288px); }
-.carousel__cell:nth-child(4) { transform: rotateY(120deg) translateZ(288px); }
-.carousel__cell:nth-child(5) { transform: rotateY(160deg) translateZ(288px); }
-.carousel__cell:nth-child(6) { transform: rotateY(200deg) translateZ(288px); }
-.carousel__cell:nth-child(7) { transform: rotateY(240deg) translateZ(288px); }
-.carousel__cell:nth-child(8) { transform: rotateY(280deg) translateZ(288px); }
-.carousel__cell:nth-child(9) { transform: rotateY(320deg) translateZ(288px); }
-
-
 
 .carousel-options {
   width: 100%;
@@ -191,5 +147,10 @@ export default {
   position: relative;
   z-index: 2;
   background: hsla(0, 0%, 100%, 0.8);
+}
+
+.cell-image {
+  width: 100%;
+  height: 100%;
 }
 </style>
