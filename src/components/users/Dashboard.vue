85a469b5de450633335ded93278c44d0288a1db0
Kirua <yo44prg@icloud.com>
2021-09-18 14:33:56 +0200

    feat: dislay comment by user

--- src/components/users/Dashboard.vue
+++ src/components/users/Dashboard.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
+  <div class="dashboard">
     <h1>Dashboard</h1>
     <h2>Your information : </h2>
     <p>your name : <span>{{ this.userInfo.login }}</span></p>
     <p>Your role : <span>{{ this.userInfo.role }}</span></p>
+    <div class="user-comments">
+      <h2>Your comments : </h2>
+      <div class="user-comment" v-for="(userComment, i) in userComments" :key="i">
+        <p>Picture title : {{ userComment.picture_title }}</p>
+        <p>Comment content : {{ userComment.content }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -54,5 +61,33 @@ export default {
 </script>
 
 <style>
+.dashboard {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
 
+.dashboard h1,
+.dashboard h2,
+.dashboard p {
+  width: 100%;
+}
+
+.user-comments {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.user-comment {
+  width: 80%;
+  background-color: #efefef;
+  border-radius: 15px;
+  text-align: left;
+  padding: 5px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
 </style>
